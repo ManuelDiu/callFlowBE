2b3e042735d3247c240e5d4a4a88f102111fc644
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2023-09-22 20:56:21 -0300

    fix login

--- src/entities/usuarios/usuarios.entity.ts
+++ src/entities/usuarios/usuarios.entity.ts
@@ -18,7 +18,7 @@ export class Usuario extends BaseEntity {
   @Unique(['email'])
   email: string;
 
-  @Column({ length: 100, nullable: false, select: false })
+  @Column({ length: 100, nullable: false, select: true })
   password: string;
 
   @Column({ length: 255, nullable: false })
@@ -66,7 +66,6 @@ export class Usuario extends BaseEntity {
   resetPasswordToken: string;
 
   toJSON() {
-    delete this.password;
     return this;
   }
 
